252d7eb31838d34697d879467c1fc8f349fe02f2
relax <1413705731@qq.com>
2021-06-17 13:13:58 +0800

    add:为viewProvider添加回调

--- src/Mod/Part/Gui/CommandLayItemWithGeo.cpp
+++ src/Mod/Part/Gui/CommandLayItemWithGeo.cpp
@@ -10,11 +10,18 @@
 #include <Gui/Application.h>
 #include <Gui/Command.h>
 #include <Gui/MainWindow.h>
+#include <Gui/View3DInventorViewer.h>
+#include <Gui/Document.h>
+
+#include <Gui/InventorAll.h>
+#include <Gui/View3DInventor.h>
 
 //===========================================================================
 // Part_LayCylinder
 //===========================================================================
-DEF_STD_CMD_A(CmdPartLayCylinder)
+DEF_STD_CMD_A_CB(CmdPartLayCylinder)
+
+//static void CmdPartLayCylinder::CBFunction(void* ud, SoEventCallback* n);
 
 CmdPartLayCylinder::CmdPartLayCylinder()
     : Command("Part_LayCylinder")
@@ -41,11 +48,45 @@ void CmdPartLayCylinder::activated(int iMsg)
         .arg(qApp->translate("CmdPartCylinder", "Cylinder"));
     runCommand(Doc, cmd.toUtf8());
 #endif
+    //1.get 3DInventorViewer
+    //Gui::View3DInventorViewer* pViewer = reinterpret_cast<Gui::View3DInventorViewer*>(getActiveGuiDocument()->getActiveView());
+
+    App::Document* activeDoc = App::GetApplication().getActiveDocument();
+    if (!activeDoc) {
+        printf("%s(%d)\n", __FUNCTION__, __LINE__);
+        return;
+    }
+    Gui::Document* activeGui = Gui::Application::Instance->getDocument(activeDoc);
+    if (!activeGui) {
+        printf("%s(%d)\n", __FUNCTION__, __LINE__);
+        return;
+    }
+
+    Gui::MDIView* pView = activeGui->getActiveView();
+    Gui::View3DInventorViewer* p3DViewer = nullptr;
+    if (pView->isDerivedFrom(Gui::View3DInventor::getClassTypeId())) {
+        printf("%s(%d)\n", __FUNCTION__, __LINE__);
+        p3DViewer = reinterpret_cast<Gui::View3DInventor*>(pView)->getViewer();
+    }
+    else {
+        printf("%s(%d)\n", __FUNCTION__, __LINE__);
+        return;
+    }
+
+
+    //2.add cb
+
+    if (p3DViewer == nullptr) {
+        printf("%s(%d)\n", __FUNCTION__, __LINE__);
+        return;
+    }
+
+    p3DViewer->addEventCallback(SoEvent::getClassTypeId(), CBFunction);
 
     printf("%s(%d)\n", __FUNCTION__, __LINE__);
     commitCommand();
     updateActive();
-    runCommand(Gui, "Gui.SendMsgToActiveView(\"ViewFit\")");
+    //runCommand(Gui, "Gui.SendMsgToActiveView(\"ViewFit\")");
 }
 
 bool CmdPartLayCylinder::isActive(void)
@@ -54,6 +95,21 @@ bool CmdPartLayCylinder::isActive(void)
         return true;
     else
         return false;
+}
+
+void  CmdPartLayCylinder::CBFunction(void* ud, SoEventCallback* n) {
+    printf("%s(%d), start\n", __FUNCTION__, __LINE__);
+    Gui::View3DInventorViewer* pViewer = reinterpret_cast<Gui::View3DInventorViewer*>(n->getUserData());
+
+    const SoEvent* pSoEvent = n->getEvent();
+    const SoType SoEventType(pSoEvent->getTypeId());
+
+    bool IsMouse = SoEventType.isDerivedFrom(SoMouseButtonEvent::getClassTypeId());
+    if (IsMouse && SO_MOUSE_PRESS_EVENT(pSoEvent, BUTTON1)) {
+        pViewer->removeEventCallback(SoEvent::getClassTypeId(), CmdPartLayCylinder::CBFunction);
+        printf("%s(%d), mouse button1 press!\n", __FUNCTION__, __LINE__);
+    }
+
 }
 
 void CreateLayPartCommands(void)
